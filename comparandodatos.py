1b90cd96324f070811ad73536e9c5e94d2c1dba0
jegomezbar <jegomezbar@gmail.com>
2022-11-23 11:32:09 -0500

    se crean ejemplos de variables y condiciones

---
+++ comparandodatos.py
@@ -0,0 +1,11 @@
+#entradas
+edadusuario=int(input("Digita tu edad: "))
+saldousuario=int(input("Digita cuanto dinero traes contigo: "))
+
+#proceso
+#pregunto por la edad
+if edadusuario >= 18: 
+    print("usted es es mayor de edad")
+else:
+    print("usted es menor de edad")
+print("Discoteca PERRO NEGRO LTDA\n")
---
+++ reto1.py
@@ -0,0 +1,17 @@
+print("**********************")
+print("HAMBURGUESAS EL CORRAL")
+print("**********************")
+
+nombreusuario=input("Digite el nombre del usuario: ")
+cedulausuario=input("Digite su cedula: ")
+tipohamburguesa=input("Digite el tipo de hamburguesa: ")
+precio=int(input("Digite el valor de la hamburguesa: "))
+cantidadhamburguesa=int(input("Digite la cantidad de hamburguesas que deseas: "))
+
+print("**********************")
+print("HAMBURGUESAS EL CORRAL")
+print("**********************")
+print(tipohamburguesa)
+print(cantidadhamburguesa)
+print(precio*cantidadhamburguesa)
+
